ee9e1c701f911a789cc53be8759fc6d4b755dec7
Daniel Sanchez Latronico <alberto.daniel.sanchez@pcunix113.fing.edu.uy>
2025-04-22 14:05:32 -0300

    Ajusto archivos ; dejo en forma de link soft bits.c bits.h apuntando a Olbigatorio_1

--- bits.h
+++ bits.h
@@ -1,14 +1,1 @@
-/* 
-bits.h
-*/
-
-int             bit(unsigned int buffer, int nb);
-int             ver_binario(unsigned int buffer, int nb, FILE* nombreArchivo);
-void            print_binario(unsigned int buffer, int nb);
-unsigned int    setbit(unsigned int buffer, int nb, int val);
-unsigned int    concatena(unsigned int buffer, unsigned int codigo, int nb);
-unsigned int    crear_mascara(int max, int min);
-unsigned int    espejar(unsigned int in, int nbits);
-unsigned int    extraer(unsigned int buffer, int min, int max);
-
-
+../OBLIGATORIO_1/bits.h
